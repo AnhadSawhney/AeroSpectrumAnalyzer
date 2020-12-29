59c5010a46a60e76b67c4b3ac2cd89e45fd21b33
Anhad <sawhney.anhad@gmail.com>
2020-12-29 15:51:42 -0500

    attack/delay fix

--- AeroSpectrumAnalyzer.cpp
+++ AeroSpectrumAnalyzer.cpp
@@ -40,9 +40,9 @@
 #define EMPTY_TIMEOUT			0.500
 #define DEVICE_TIMEOUT			1.500
 #define QUERY_TIMEOUT			(1.0/60)
-#define xres 128
+#define xres 64
 #define yres 64
-#define BANDS 128
+#define BANDS 256
 
 enum Port {
 	PORT_OUTPUT,
@@ -59,21 +59,6 @@ enum Port {
 	CHANNEL_SR,
 	MAX_CHANNELS,
 	CHANNEL_SUM = MAX_CHANNELS
-};
-enum Type {
-	TYPE_RMS,
-	TYPE_PEAK,
-	TYPE_FFT, <----- ALWAYS THIS
-	TYPE_BAND,
-	TYPE_FFTFREQ,
-	TYPE_BANDFREQ,
-	TYPE_FORMAT,
-	TYPE_DEV_STATUS,
-	TYPE_DEV_NAME,
-	TYPE_DEV_ID,
-	TYPE_DEV_LIST,
-	// ... //
-	NUM_TYPES
 };*/
 enum Format {
 	FMT_INVALID,
@@ -94,16 +79,11 @@ Port					m_port = PORT_OUTPUT;						// port specifier (parsed from options)
 //Channel					m_channel = LEFT;					// channel specifier (parsed from options)
 //Type					m_type = TYPE_FFT;						// data type specifier (parsed from options)
 Format					m_format = FMT_PCM_F32;					// format specifier (detected in init)
-//int						m_envRMS[2];				// RMS attack/decay times in ms (parsed from options)
-//int						m_envPeak[2];				// peak attack/decay times in ms (parsed from options)
-int						m_envFFT[2] = { 300, 300 };				// FFT attack/decay times in ms (parsed from options), 15, 250
-int						m_fftSize = 1024;					// size of FFT (parsed from options), powers of 2 work best
+int						m_envFFT[2] = { 25,250 };				// FFT attack/decay times in ms (parsed from options), 15, 250
+int						m_fftSize = 256;					// size of FFT (parsed from options), powers of 2 work best
 int						m_fftOverlap = 0;				// number of samples between FFT calculations, between 0 and fftSize
-//int						m_nBands = BANDS;					// number of frequency bands (parsed from options)
-//double					m_gainRMS;					// RMS gain (parsed from options)
-//double					m_gainPeak;					// peak gain (parsed from options)
 double					m_freqMin = 20;					// min freq for band measurement
-double					m_freqMax = 20000;					// max freq for band measurement
+double					m_freqMax = 10000;					// max freq for band measurement
 double					m_sensitivity = 100.0;				// dB range for FFT/Band return values (parsed from options), min 1
 
 IMMDeviceEnumerator* m_enum;						// audio endpoint enumerator
@@ -122,17 +102,14 @@ const IID IID_IAudioRenderClient = __uuidof(IAudioRenderClient);
 IAudioClient* m_clBugAudio;				// audio client for dummy silent channel
 IAudioRenderClient* m_clBugRender;				// render client for dummy silent channel
 #endif
-//WCHAR					m_reqID[64];				// requested device ID (parsed from options)
 WCHAR					m_devName[64];				// device friendly name (detected in init)
-//float					m_kRMS[2];					// RMS attack/decay filter constants
-//float					m_kPeak[2];					// peak attack/decay filter constants
 float					m_kFFT[2];					// FFT attack/decay filter constants
 double					m_pcMult;					// performance counter inv frequency
 LARGE_INTEGER			m_pcFill;					// performance counter on last full buffer
 LARGE_INTEGER			m_pcPoll;					// performance counter on last device poll
-kiss_fftr_cfg			m_fftCfg;// [MAX_CHANNELS] ;		// FFT states for each channel
-float* m_fftIn;// [MAX_CHANNELS] ;		// buffer for each channel's FFT input
-float* m_fftOut;// [MAX_CHANNELS] ;		// buffer for each channel's FFT output
+kiss_fftr_cfg			m_fftCfg;		// FFT states for each channel
+float* m_fftIn;		// buffer for each channel's FFT input
+float* m_fftOut;		// buffer for each channel's FFT output
 float* m_fftKWdw;					// window function coefficients
 float* m_fftTmpIn;					// temp FFT processing buffer
 kiss_fft_cpx* m_fftTmpOut;				// temp FFT processing buffer
@@ -260,6 +237,13 @@ HRESULT	DeviceInit() {
 		std::cerr << "WARNING: Invalid sample format.  Only PCM 16b integer or PCM 32b float are supported.\n";
 	}
 
+	// regenerate filter constants
+	if (m_wfx) {
+		const double freq = m_wfx->nSamplesPerSec;
+		m_kFFT[0] = (float)exp(log10(0.01) / (freq / (m_fftSize - m_fftOverlap) * (double)m_envFFT[0] * 0.001));
+		m_kFFT[1] = (float)exp(log10(0.01) / (freq / (m_fftSize - m_fftOverlap) * (double)m_envFFT[1] * 0.001));
+	}
+
 	// setup FFT buffers
 	m_fftCfg = kiss_fftr_alloc(m_fftSize, 0, NULL, NULL);
 	m_fftIn = (float*)calloc(m_fftSize * sizeof(float), 1);
@@ -361,9 +345,7 @@ Exit:
 	return hr;
 }
 
-
 void Initialize() {
-	//m_reqID[0] = '\0';
 	m_devName[0] = '\0';
 	for (int i = 0; i < BANDS; i++) {
 		m_bandOut[i] = 0;
@@ -381,17 +363,6 @@ void Initialize() {
 		return;
 	}
 
-	// regenerate filter constants
-	if (m_wfx) {
-		const double freq = m_wfx->nSamplesPerSec;
-		//		m_kRMS[0] = (float)exp(log10(0.01) / (freq * (double)m_envRMS[0] * 0.001));
-		//		m_kRMS[1] = (float)exp(log10(0.01) / (freq * (double)m_envRMS[1] * 0.001));
-		//		m_kPeak[0] = (float)exp(log10(0.01) / (freq * (double)m_envPeak[0] * 0.001));
-		//		m_kPeak[1] = (float)exp(log10(0.01) / (freq * (double)m_envPeak[1] * 0.001));
-		m_kFFT[0] = (float)exp(log10(0.01) / (freq / (m_fftSize - m_fftOverlap) * (double)m_envFFT[0] * 0.001));
-		m_kFFT[1] = (float)exp(log10(0.01) / (freq / (m_fftSize - m_fftOverlap) * (double)m_envFFT[1] * 0.001));
-	}
-
 	SAFE_RELEASE(m_enum)
 }
 
@@ -400,59 +371,15 @@ void Update() {
 	QueryPerformanceCounter(&pcCur);
 	//UINT32			n;
 	//m_clCapture->GetNextPacketSize(&n); //441
-	//std::cout << "in update\n" << n;
 
 	// query the buffer
 	if (m_clCapture && ((pcCur.QuadPart - m_pcPoll.QuadPart) * m_pcMult) >= QUERY_TIMEOUT) {
-		//std::cout << "Got clcapture\n";
 		BYTE* buffer;
 		UINT32			nFrames;
 		DWORD			flags;
 		UINT64			pos;
 		HRESULT			hr;
 		while ((hr = m_clCapture->GetBuffer(&buffer, &nFrames, &flags, &pos, NULL)) == S_OK) {
-			//std::cout << "Got buffer\n";
-			// measure RMS and peak levels
-			/*float		rms[Measure::MAX_CHANNELS];
-			float		peak[Measure::MAX_CHANNELS];
-			for (int iChan = 0; iChan < Measure::MAX_CHANNELS; ++iChan) {
-				rms[iChan] = (float)m_rms[iChan];
-				peak[iChan] = (float)m_peak[iChan];
-			}
-			// loops unrolled for float, 16b and mono, stereo
-			if (m_format == Measure::FMT_PCM_F32) {
-				float* s = (float*)buffer;
-				if (m_wfx->nChannels == 1) {
-					for (unsigned int iFrame = 0; iFrame < nFrames; ++iFrame) {
-						float xL = (float)*s++;
-						float sqrL = xL * xL;
-						float absL = abs(xL);
-						rms[0] = sqrL + m_kRMS[(sqrL < rms[0])] * (rms[0] - sqrL);
-						peak[0] = absL + m_kPeak[(absL < peak[0])] * (peak[0] - absL);
-						rms[1] = rms[0];
-						peak[1] = peak[0];
-					}
-				}
-			} else if (m_format == Measure::FMT_PCM_S16) {
-				INT16* s = (INT16*)buffer;
-				if (m_wfx->nChannels == 1) {
-					for (unsigned int iFrame = 0; iFrame < nFrames; ++iFrame) {
-						float xL = (float)*s++ * 1.0f / 0x7fff;
-						float sqrL = xL * xL;
-						float absL = abs(xL);
-						rms[0] = sqrL + m_kRMS[(sqrL < rms[0])] * (rms[0] - sqrL);
-						peak[0] = absL + m_kPeak[(absL < peak[0])] * (peak[0] - absL);
-						rms[1] = rms[0];
-						peak[1] = peak[0];
-					}
-				}
-			}
-			for (int iChan = 0; iChan < Measure::MAX_CHANNELS; ++iChan) {
-				m_rms[iChan] = rms[iChan];
-				m_peak[iChan] = peak[iChan];
-			}*/
-
-			// process FFTs (optional)
 			float* sF32 = (float*)buffer;
 			INT16* sI16 = (INT16*)buffer;
 			const float	scalar = (float)(1.0 / sqrt(m_fftSize));
@@ -466,7 +393,7 @@ void Update() {
 						// copy from the ring buffer to temp space
 						memcpy(&m_fftTmpIn[0], &(m_fftIn)[m_fftBufW], (m_fftSize - m_fftBufW) * sizeof(float));
 						memcpy(&m_fftTmpIn[m_fftSize - m_fftBufW], &m_fftIn[0], m_fftBufW * sizeof(float));
-						// apply the windowing function
+						// apply the windowing function and perform FFT
 						for (int iBin = 0; iBin < m_fftSize; ++iBin) {
 							m_fftTmpIn[iBin] *= m_fftKWdw[iBin];
 						}
@@ -518,22 +445,22 @@ void Update() {
 		}
 		// detect device disconnection
 		switch (hr) {
-		case AUDCLNT_S_BUFFER_EMPTY:
-			// Windows bug: sometimes when shutting down a playback application, it doesn't zero
-			// out the buffer.  Detect this by checking the time since the last successful fill
-			// and resetting the volumes if past the threshold.
-			/*if (((pcCur.QuadPart - m_pcFill.QuadPart) * m_pcMult) >= EMPTY_TIMEOUT) {
-				for (int iChan = 0; iChan < Measure::MAX_CHANNELS; ++iChan) {
-					m_rms[iChan] = 0.0;
-					m_peak[iChan] = 0.0;
-				}
-			}*/
-			break;
-		case AUDCLNT_E_BUFFER_ERROR:
-		case AUDCLNT_E_DEVICE_INVALIDATED:
-		case AUDCLNT_E_SERVICE_NOT_RUNNING:
-			DeviceRelease();
-			break;
+			case AUDCLNT_S_BUFFER_EMPTY:
+				// Windows bug: sometimes when shutting down a playback application, it doesn't zero
+				// out the buffer.  Detect this by checking the time since the last successful fill
+				// and resetting the volumes if past the threshold.
+				/*if (((pcCur.QuadPart - m_pcFill.QuadPart) * m_pcMult) >= EMPTY_TIMEOUT) {
+					for (int iChan = 0; iChan < Measure::MAX_CHANNELS; ++iChan) {
+						m_rms[iChan] = 0.0;
+						m_peak[iChan] = 0.0;
+					}
+				}*/
+				break;
+			case AUDCLNT_E_BUFFER_ERROR:
+			case AUDCLNT_E_DEVICE_INVALIDATED:
+			case AUDCLNT_E_SERVICE_NOT_RUNNING:
+				DeviceRelease();
+				break;
 		}
 		m_pcPoll = pcCur;
 	} else if (!m_clCapture && (((pcCur.QuadPart - m_pcPoll.QuadPart) * m_pcMult) >= DEVICE_TIMEOUT)) {
@@ -543,50 +470,6 @@ void Update() {
 		DeviceInit();
 		m_pcPoll = pcCur;
 	}
-
-	/*switch (m_type) {
-		case Measure::TYPE_FFT:
-			if (parent->m_clCapture && parent->m_fftSize) {
-				double	x;
-				const int iFFT = m_fftIdx;
-				if (m_channel == Measure::CHANNEL_SUM) {
-					if (parent->m_wfx->nChannels >= 2) {
-						x = (parent->m_fftOut[0][iFFT] + parent->m_fftOut[1][iFFT]) * 0.5;
-					} else {
-						x = parent->m_fftOut[0][iFFT];
-					}
-				} else if (m_channel < parent->m_wfx->nChannels) {
-					x = parent->m_fftOut[m_channel][iFFT];
-				}
-				x = CLAMP01(x);
-				x = max(0, 10.0 / parent->m_sensitivity * log10(x) + 1.0);
-				return x;
-			}
-			break;
-		case Measure::TYPE_BAND:
-			if (parent->m_clCapture && parent->m_nBands) {
-				double	x;
-				const int iBand = m_bandIdx;
-				if (m_channel == Measure::CHANNEL_SUM) {
-					if (parent->m_wfx->nChannels >= 2) {
-						x = (parent->m_bandOut[0][iBand] + parent->m_bandOut[1][iBand]) * 0.5;
-					} else {
-						x = parent->m_bandOut[0][iBand];
-					}
-				} else if (m_channel < parent->m_wfx->nChannels) {
-					x = parent->m_bandOut[m_channel][iBand];
-				}
-				x = CLAMP01(x);
-				x = max(0, 10.0 / parent->m_sensitivity * log10(x) + 1.0);
-				return x;
-			}
-			break;
-		case Measure::TYPE_FFTFREQ:
-			if (parent->m_clCapture && parent->m_fftSize && m_fftIdx <= (parent->m_fftSize / 2)) {
-				return (m_fftIdx * m_wfx->nSamplesPerSec / parent->m_fftSize);
-			}
-			break;
-	}*/
 }
 
 
@@ -741,17 +624,19 @@ int main() {
 
 		float max = 0;
 
-		for (int i = 1; i < BANDS; ++i) {
-			if (m_bandOut[i] > max) {
-				max = m_bandOut[i];
+		for (int i = 1; i < xres; ++i) {
+			if (m_fftOut[i] > max) {
+				max = m_fftOut[i];
 			}
 		}
 
+		max = 2;
+
 		std::cout << max << std::endl;
 
-		for (int i = 0; i < BANDS; i++) {
+		for (int i = 0; i < xres; i++) {
 			int j = 0;
-			for (; j < m_bandOut[i] / max * yres; j++) {
+			for (; j < m_fftOut[i] / max * yres && j < yres; j++) {
 				R[i][j][0] = 255; //BGR
 				R[i][j][1] = 0;
 				R[i][j][2] = 0;
